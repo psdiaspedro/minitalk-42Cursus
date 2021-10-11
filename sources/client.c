b76c6e5614d997c7f5e0eb1ca68b505ff690bf75
Pedro Dias <pedroadias@MacBook-Pro-de-Pedro.local>
2021-10-11 00:20:32 -0300

    update

--- sources/client.c
+++ sources/client.c
@@ -1,6 +1,39 @@
 #include "../include/minitalk.h"
 # include <stdio.h>
 
+void send_char (int pid, unsigned char byte)
+{
+	//verificar bit
+	int i;
+	int mask;
+
+
+	i = 0;
+	mask = 1;
+	while (i < 8)
+	{
+		if (byte & mask)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		mask << 1;
+		i++;
+	}
+}
+
+void send_message(pid_t pid, char *string)
+{
+	int i;
+	char letra;
+
+	i = 0;
+	while (string[i] != '\0')
+	{
+		send_char (pid, string[i]);
+		i++;
+	}
+	send_char (pid, string[i])
+}
 
 int	main(int argc, char **argv)
 {
@@ -19,7 +52,7 @@ int	main(int argc, char **argv)
 			ft_printf("Valor do PID invalido");
 			return (1);
 		}
-		kill(pid, SIGUSR1);
+		send_message(pid, argv[2]);
 	}
 	return (0);
 }
--- sources/server.c
+++ sources/server.c
@@ -1,13 +1,17 @@
 #include "../include/minitalk.h"
 
-
 void my_handler(int signum, siginfo_t *info, void *ucontext)
 {
 	(void)ucontext;
     if (signum == SIGUSR1)
 	{
         ft_printf("Received SIGUSR1!\n");
-		ft_printf("Client PID: %d\n", info->si_pid);
+		//ft_printf("Client PID: %d\n", info->si_pid);
+	}
+	if (signum == SIGUSR2)
+	{
+        ft_printf("Received SIGUSR2!\n");
+		//ft_printf("Client PID: %d\n", info->si_pid);
 	}
 
 }
@@ -15,16 +19,20 @@ void my_handler(int signum, siginfo_t *info, void *ucontext)
 int	main(void)
 {
 	struct sigaction act;
+	//sigset_t mask_set;
 	pid_t	pid;
 
+	// sigemptyset(&mask_set);
+	// sigaddset(&mask_set, SIGTSTP);
+	// sigaddset(&mask_set, SIGINT);
 	act.sa_handler = NULL;
 	act.sa_flags = SA_SIGINFO;
+	//act.sa_mask = mask_set;
 	act.sa_sigaction = my_handler;
-
-
 	pid = getpid();
 	ft_printf("Esse é o PID: %d\n", pid);
 	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
 	while (1)
 		pause();
 	return (0);
@@ -32,10 +40,10 @@ int	main(void)
 
 /*
 	struct sigaction {
-		sa_handler -> ponteiro para a função de manipulação
-		sa_sigaction -> um ponteiro para a função de manipulação (habilitada com SA_SIGINFO)
-		sa_flags -> SA_SIGINFO (habilita a configuração do sa_sigaction)
-		sa_mask;
+		void *sa_handler -> ponteiro para a função de manipulação
+		void *sa_sigaction -> um ponteiro para a função de manipulação (habilitada com SA_SIGINFO)
+		int sa_flags -> SA_SIGINFO (habilita a configuração do sa_sigaction)
+		sigset_t sa_mask;
 	}
 */
 
