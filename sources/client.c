36e593f9fd960056d9fee913addeee6d3e57ccdf
Pedro Dias <psdias.pedro@gmail.com>
2021-10-13 21:49:34 -0300

    update

---
+++ sources/client.c
@@ -0,0 +1,66 @@
+#include "../include/minitalk.h"
+
+static void	handle(int sig, siginfo_t *siginfo, void *context)
+{
+	(void)siginfo;
+	(void)context;
+	if (sig == SIGUSR1)
+	{
+		ft_printf("Message Recived Successfully\n");
+		exit(0);
+	}
+}
+
+static void	ft_kill(int server_pid, int signal)
+{
+	kill(server_pid, signal);
+	usleep(10000);
+}
+
+static void	send_char(int server_pid, unsigned char c)
+{
+	int	count;
+
+	count = -1;
+	while (++count < 8)
+	{
+		if (c % 2 == 0)
+			ft_kill(server_pid, SIGUSR1); //0
+		else
+			ft_kill(server_pid, SIGUSR2); //1
+		c /= 2;
+	}
+}
+
+static void send_message(int server_pid, const char *message)
+{
+	unsigned int i;
+
+	i = 0;
+	while (message[i] != '\0')
+	{
+		send_char(server_pid, message[i]);
+		i++;
+	}
+	send_char(server_pid, message[i]);
+}
+
+int	main(int argc, char *argv[])
+{
+	int					server_pid;
+	struct sigaction	act;
+
+	if (argc != 3)
+	{
+		ft_printf("Wrong input. Must be: ./client + Server PID + Message\n");
+		return (1);
+	}
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = handle;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	server_pid = ft_atoi(argv[1]);
+	send_message(server_pid, argv[2]);
+	while (1)
+		pause();
+}
---
+++ sources/server.c
@@ -0,0 +1,62 @@
+#include "../include/minitalk.h"
+
+static void	ft_finish(int pid)
+{
+	ft_printf("\n");
+	if (kill(pid, SIGUSR1))
+		ft_printf("Error to notify client\n");
+}
+
+static void	recive_message(int sig, siginfo_t *siginfo, void *context)
+{
+	static unsigned int	count;
+	static unsigned int	letter;
+
+	(void)context;
+	if (count > 7)
+	{
+		count = 0;
+		letter = 0;
+	}
+	if (sig == SIGUSR2)
+		letter += 1 << count;
+	count++;
+	if (count == 8)
+	{
+		if (letter == 0)
+		{
+			ft_finish(siginfo->si_pid);
+			return ;
+		}
+		else
+			ft_printf("%c", letter);
+	}
+	kill(siginfo->si_pid, SIGUSR2);
+}
+
+int	main(void)
+{
+	struct sigaction	act;
+
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = recive_message;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	ft_printf("[SERVER] > listening on pid %d\n", getpid());
+	while (1)
+		pause();
+}
+
+/*
+	struct sigaction {
+		void *sa_sigaction -> um ponteiro para a função de manipulação (habilitada com SA_SIGINFO)
+		int sa_flags -> SA_SIGINFO (habilita a configuração do sa_sigaction)
+		sigset_t sa_mask;
+	}
+*/
+
+/*
+	struct siginfo_t {
+		pid_t si_pid -> sending processes ID -> IMPORTANTE
+	}
+*/
